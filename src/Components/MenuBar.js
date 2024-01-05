70bb024f7897a7b52cae1c7f9b289b65a4760acd
Nataniel Jayaseelan <63678990+Ironspidr@users.noreply.github.com>
2024-01-04 17:07:36 -0800

    menu bar fix

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -4,12 +4,12 @@ export default function MenuBar() {
   return (
     <div>
       <Breadcrumbs separator=" | ">
-        <a href="/">2023</a>
-        <a href="/2022">2022</a>
-        <a href="/2021">2021</a>
-        <a href="/2020">2020</a>
-        <a href="/2019">2019</a>
-        <a href="/2018">2018</a>
+        <a href="/CompetitiveHistory/">2023</a>
+        <a href="/CompetitiveHistory/2022">2022</a>
+        <a href="/CompetitiveHistory/2021">2021</a>
+        <a href="/CompetitiveHistory/2020">2020</a>
+        <a href="/CompetitiveHistory/2019">2019</a>
+        <a href="/CompetitiveHistory/2018">2018</a>
       </Breadcrumbs>
     </div>
   );
